1cf4ddb18d0097270fc9b904107ff1d986603562
azkaa23 <muhamadazkamubarok@smkwikrama.sch.id>
2023-11-01 08:01:43 +0700

    vue component

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- jumlah card sama dengan jumlah data cardContents -->
+  <Card v-for="(item, index) in cardContents" :key="index" :judul="item.title" :text="item.text"
+  :image="item.image">
+    <!-- kodingan yang ada ditengah component bakal masuk ke slot -->
+    <small>created by: orang pinggiran</small>
+  </Card>
+</template>
+<script>
+//panggil file
+import Card from '@/components/Card.vue';
+
+export default{
+  //component apa saja yang akan ditampilkan
+  components: {
+    //definisi component agar bisa digunakan di template
+    Card,
+  },
+  //penyimpanan variable
+  data() {
+    return{
+    cardContents: [
+      {
+        title: 'Cara menyusahkan orang tua :',
+        text: `<ul>
+                    <li>Main ml sampe subuh</li>
+                    <li>minta kuota buat main ml</li>
+                    <li>keluar kamar hanya untuk makan</li>
+                </ul>`,
+                image: "img.png",
+      },
+      {
+        title: 'Cara Menjadi Pengangguran :',
+        text: `<ul>
+                    <li>bangun tidur langsung tidur lagi</li>
+                    <li>main ml 24 jam,mandi cukup sekali saja</li>
+                    <li>Perbanyak melamun,bangun siang,begadang</li>
+                </ul>`,
+                image:"img1.png",
+      }
+    ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="wrap">
+        <div class="card">
+            <div class="img-wrap">
+                <img :src="'/src/assets/img/'+image" alt="" srcset="">
+            </div>
+            <div class="text-wrap">
+                <h3>{{ judul }}</h3>
+                <slot></slot>
+                <div v-html="text">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        //mengambil data dari luar atau file lain
+        props: ['judul','text', 'image'],
+    }
+</script>
+
+<style scoped>
+.wrap{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 25px;
+}
+.card{
+    width: 600px;
+    height: auto;
+    border-radius: 5px;
+    border: 1px solid black;
+    display: flex;
+}
+.img-wrap {
+    width: 35%;
+    height: auto;
+    position: relative;
+}
+
+.text-wrap {
+    width:65%;
+    height: 100%;
+    padding: 10px;
+}
+
+.img-wrap img{
+    width: 100%;
+    height: 100%;
+    /* position: absolute; */
+}
+</style>
